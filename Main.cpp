2ca6e9b41fb4c174cf3e3f6abd6a39694f7ef8bb
jmppmj <jillplatts@gmail.com>
2020-02-24 15:28:22 -0500

    02/24/20

--- Main.cpp
+++ Main.cpp
@@ -38,8 +38,8 @@ GLOBALS
 ///////////////////////////////////////////////////////////////////////////////*/
 
 GLFWwindow *window = NULL;
-int windowWidth = 800;
-int windowHeight = 600;
+int windowWidth = 1920;
+int windowHeight = 1080;
 
 int framebufferWidth = windowWidth;
 int framebufferHeight = windowHeight;
@@ -317,7 +317,7 @@ int main(int argc, char **argv) {
 	
 	// GLEW setup //
 	
-	// MAC-SPECIFIC: Some issues occur with using OpenGL core and GLEW; so, we'll use the experimental version of GLEW
+	// MAC-SPECIFIC: Use the experimental version of GLEW
 	glewExperimental = true;
 
 	// (Try to) initalize GLEW
@@ -352,24 +352,8 @@ int main(int argc, char **argv) {
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
 	ImGui_ImplOpenGL3_Init(glsl_version);
 
-	// Load Fonts
-	// - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-	// - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-	// - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-	// - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-	// - Read 'docs/FONTS.txt' for more instructions and details.
-	// - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-	//io.Fonts->AddFontDefault();
-	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
-	//ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-	//IM_ASSERT(font != NULL);
-
 	// Our state
-	bool show_demo_window = true;
-	bool show_another_window = false;
+	bool show_another_window = true;
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 	/*/////////////////////////////////////////////////////////////////////////////*/
 
@@ -456,37 +440,39 @@ int main(int argc, char **argv) {
 	shader->setMaterialChoice(0);
 	
 	while (!glfwWindowShouldClose(window)) {
-		// Poll and handle events (inputs, window resize, etc.)
-		// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-		// - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-		// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-		// Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
 		glfwPollEvents();
+		//io.MousePos = my_mouse_pos;
 
 		// Start the Dear ImGui frame
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
-		// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-		if (show_demo_window)
-			ImGui::ShowDemoWindow(&show_demo_window);
+		ImGui::SetMouseCursor(ImGuiMouseCursor_None);
+		ImGui:(io.WantCaptureMouse, true);
 
-		// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+		/*With previous implementations then only relevant variable to tell who draws the cursor was io.MouseDrawCursor:
+
+		imgui: io.MouseDrawCursor is true
+		glfw : io.MouseDrawCursor is false
+		Now NoMouseCursorChange is also relevant :
+
+		imgui: io.MouseDrawCursor is true and NoMouseCursorChange is false
+		glfw : io.MouseDrawCursor is false and NoMouseCursorChange is true*/
+
+		// 1st window
 		{
 			static float f = 0.0f;
 			static int counter = 0;
 
-			ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+			ImGui::Begin("Menu");                                    // Create a window called "Menu" and append into it.
+			ImGui::Text("This is some useful text.");                // Display some text (you can use a format strings too)
+			ImGui::Checkbox("Another Window", &show_another_window); // Edit bools storing our window open/close state
 
-			ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-			ImGui::Checkbox("Another Window", &show_another_window);
+			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // float using a slider from 0.0f to 1.0f
+			ImGui::ColorEdit3("clear color", (float*)&clear_color);  // floats representing a color
 
-			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-			if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+			if (ImGui::Button("Button"))                             // Buttons return true when clicked (most widgets return true when edited/activated)
 				counter++;
 			ImGui::SameLine();
 			ImGui::Text("counter = %d", counter);
@@ -495,10 +481,10 @@ int main(int argc, char **argv) {
 			ImGui::End();
 		}
 
-		// 3. Show another simple window.
+		// 2nd window
 		if (show_another_window)
 		{
-			ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+			ImGui::Begin("Another Window", &show_another_window);
 			ImGui::Text("Hello from another window!");
 			if (ImGui::Button("Close Me"))
 				show_another_window = false;
